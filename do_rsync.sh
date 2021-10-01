6cb77f809682fab60edc2c1bddb1282f7822244e
Paul Kennedy <paul.kennedy124@gmail.com>
2021-09-30 20:43:29 -0500

    Updated do_rsync script to copy a file indicating the revision currently deployed

--- do_rsync.sh
+++ do_rsync.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+CHANGES=`git status | grep "Changes not staged for commit\|Changes to be committed"`
+
+if [ ! -z "${CHANGES}" ]; then
+    echo "you must commit changes before deploying" >&2
+    exit 1;
+fi
+
+USER="paulkenn"
+HOST="paulmkennedy.com"
+DEST_PATH="~/www/mythyme/"
+DEST="${USER}@${HOST}:${DEST_PATH}"
+
+RSYNC_FLAGS="-avzh --delete --progress"
+#RSYNC_FLAGS+=" --dry-run"
+
 EXCLUDES=()
 EXCLUDES+=(--exclude ".*")
 EXCLUDES+=(--exclude old)
@@ -7,4 +22,10 @@ EXCLUDES+=(--exclude do_rsync.sh)
 EXCLUDES+=(--exclude sql_connect.sh)
 EXCLUDES+=(--exclude README.md)
 
-rsync -avzh --delete --progress "${EXCLUDES[@]}" ./ paulkenn@paulmkennedy.com:~/www/mythyme/
+# create revision.txt and copy it to the server so we know what version is deployed
+BRANCH=`grep -oP '(?<=ref: refs\/heads\/).*' .git/HEAD`
+echo -n "$BRANCH " > revision.txt
+cat .git/refs/heads/$BRANCH >> revision.txt
+
+rsync ${RSYNC_FLAGS} "${EXCLUDES[@]}" ./ "${DEST}"
+rm revision.txt
